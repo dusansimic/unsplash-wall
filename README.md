aa17443a87ce6dd1ebf4afd1fc5c229cc051f14d
Dušan Simić <dusan.simic1810@gmail.com>
2017-01-04 04:54:11 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # unsplash-wall
-Simple random wallpaper setting using unsplash.com.
+## a simple app for setting beautiful random wallpapers using unsplash.com
+
+It's made 100% in node.js and it's open source.
+Feel free to review the code.
+
+If you want to run the code, you will need to install dependencies first.
+To do that just run `npm install`.
---
+++ main.js
@@ -0,0 +1,36 @@
+var wallpaper = require('wallpaper');
+var fs = require('fs'),
+    request = require('request');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+var main = function() {
+  wallSource = "random";
+  wallRes = "1920x1080";
+  if (process.argv.length > 2) {
+    if (process.argv[2] === "-s") {
+      wallSource = process.argv[3];
+    } else if (process.argv[2] === "-d") {
+      wallRes = process.argv[3];
+    }
+
+    if (process.argv[4] === "-d") {
+      wallRes = process.argv[5];
+    }
+  }
+
+  download('https://source.unsplash.com/' + wallSource + '/' + wallRes, 'wall.jpg', function(){
+    console.log('download ... done');
+  });
+
+  wallpaper.set('wall.jpg').then(console.log('setting ... done'));
+}
+
+main();
