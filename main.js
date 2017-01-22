61f33dac4f64023c90fa5949fbc576888b4693d3
Dušan Simić <dusan.simic1810@gmail.com>
2017-01-22 16:31:05 +0100

    working on cl args

--- main.js
+++ main.js
@@ -11,27 +11,59 @@ var download = function(uri, filename, callback){
   });
 };
 
-var main = function() {
-  wallSource = "random";
-  wallRes = "1920x1080";
-  // get command line args
-  if (process.argv.length > 2) {
-    if (process.argv[2] === "-s") {
-      wallSource = process.argv[3];
-    } else if (process.argv[2] === "-d") {
-      wallRes = process.argv[3];
-    }
+var printHelp = function() {
+  console.log();
+  console.log("   -h              | print help");
+  console.log("   -s <source>     | enter wallpaper source");
+  console.log("   -r <resolution> | enter wallpaper resolution");
+  console.log();
+}
 
-    if (process.argv[4] === "-d") {
-      wallRes = process.argv[5];
+var main = function() {
+  var wallSource = "random";
+  var wallRes = "1920x1080";
+  var allowDownload = false;
+  // get command line args
+  if (process.argv.length > 1) {
+    for (var i = 2; i <= process.argv.length; i++) {
+      console.log("entering");
+      console.log(process.argv[i]);
+      console.log(i);
+      if (process.argv[i] === "-h") {
+        console.log("help");
+        printHelp();
+        return;
+      }
+      if (process.argv[i] === "-s") {
+        console.log("source");
+        i++;
+        wallSource = process.argv[i];
+        allowDownload = true;
+        return;
+      }
+      if (process.argv[i] === "-r") {
+        console.log("resolution");
+        i++;
+        wallRes = process.argv[i];
+        allowDownload = true;
+        return;
+      }
+      if (process.argv[i] === "--use-default") {
+        console.log("use default");
+        allowDownload = true;
+        return;
+      }
+      console.log("exiting");
     }
   }
   // start the download
-  download('https://source.unsplash.com/' + wallSource + '/' + wallRes, 'wall.jpg', function(){
-    console.log('download ... done');
-    // after download is done set the wallpaper
-    wallpaper.set('wall.jpg').then(console.log('setting ... done'));
-  });
+  if (allowDownload) {
+    download('https://source.unsplash.com/' + wallSource + '/' + wallRes, 'wall.jpg', function(){
+      console.log('download ... done');
+      // after download is done set the wallpaper
+      wallpaper.set('wall.jpg').then(console.log('setting ... done'));
+    });
+  }
 }
 
 main();
