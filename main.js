b76aa0cc6561a09dda301f40678d78787505f9b9
Dušan Simić <dusan.simic1810@gmail.com>
2017-01-26 22:22:19 +0100

    patched up some things

--- main.js
+++ main.js
@@ -26,34 +26,22 @@ var main = function() {
   // get command line args
   if (process.argv.length > 1) {
     for (var i = 2; i <= process.argv.length; i++) {
-      console.log("entering");
-      console.log(process.argv[i]);
-      console.log(i);
       if (process.argv[i] === "-h") {
-        console.log("help");
         printHelp();
-        return;
       }
       if (process.argv[i] === "-s") {
-        console.log("source");
         i++;
         wallSource = process.argv[i];
         allowDownload = true;
-        return;
       }
       if (process.argv[i] === "-r") {
-        console.log("resolution");
         i++;
         wallRes = process.argv[i];
         allowDownload = true;
-        return;
       }
       if (process.argv[i] === "--use-default") {
-        console.log("use default");
         allowDownload = true;
-        return;
       }
-      console.log("exiting");
     }
   }
   // start the download
